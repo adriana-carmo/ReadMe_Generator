2fdcf67bb7e00f76b1bfbefe22c96dfe401b5d03
Adriana <dri.abedala@gmail.com>
2020-10-07 15:31:17 -0400

    Include more information to GeneratorMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,32 +1,63 @@
 // function to generate markdown for README
+
+// * Title
+//   * Description
+//   * Table of Contents
+//   * Installation
+//  * Usage
+//  * License
+//  * Contributing
+//  * Tests
+//  * Questions
+
 function generateMarkdown(data) {
-  return `# ${data.title}
+   return `# ${data.title}
+
+
+  ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/${data.username}/${data.repo}?style=flat&logo=appveyor) 
+  ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${data.username}/${data.repo}?style=flat&logo=appveyor)
+  
+  Check out the badges hosted by [shields.io](https://shields.io/).
 
   #Table of Contents:
-  -[Description](#description)
-  -[Contribution](#contribution)
-  -[Installation](#installation)
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contribution)
+  - [Test](#test)
+  - [Question](#question) 
   
-  ##Description
-  ${data.description}
+  
+  ## Description <a name="description"></a>
+    ${data.description}
 
-  ##Contribution 
-  ${data.contribution}
+  ## Installation <a name="installation"></a>
+    ${data.installation}
 
-  ##Installation 
-  ${data.installation}
+  ## Usage
+    ${data.usage}
 
-  ##License
+  ## License
   For more information about the license, click on the link below:
   - [License](https://opensource.org/licenses/${data.license}) 
 
-  ##Question
+  ## Contributing
+    ${data.contribution}
+
+  ## Test
+    ${data.test}
+
+  ##  Question <a name="question"></a>
   For more questions about Generator you can go to my Github page at the follow link: 
 
-  - [GitHub Profile]("https://github.com/"${data.title})
+  - [GitHub Profile](https://github.com/${data.username})
+  - [GitHub Repo](https://github.com/${data.username}/${data.repo})
 
-  For additional questions please reach out to my e-mail at : ${data.title}
+  For additional questions please reach out to my e-mail at : ${data.email}
 
 `;
 }
+
+module.exports = generateMarkdown;
 
